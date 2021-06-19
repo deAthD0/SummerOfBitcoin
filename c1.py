2646f3220fe5ffc50d3d53870687b535d4da5485
deAthD0 <dhruvarora26@gmail.com>
2021-06-19 21:41:11 +0530

    first commit

---
+++ c1.py
@@ -0,0 +1,95 @@
+import pandas as pd
+
+global data
+data = pd.read_csv("mempool.csv") 
+# data= data.sort_values(by='fee',ascending=False) 
+
+
+def check_list(row):
+    if str(row) in df:
+        return True
+    else:
+        return False
+
+def checkParents(row):
+    global df, total_weight,current_fees, max_weight
+    if(str(data.at[row,'parents '])!="nan"):
+        list_of_parents=data.at[row,'parents '].split(";")
+        for i in list_of_parents:   
+            if(check_list(i)==True):
+                continue
+            else:
+                if(total_weight+data.at[row,'weight']<=max_weight):
+                    temp=data.index[data['tx_id'] == str(i)]
+                    nRow=temp[0]
+                    total_weight=total_weight+data.at[nRow,'weight']
+                    current_fees=current_fees+data.at[nRow,'fee']
+                    new_Row={'tx_id':data.at[nRow,'tx_id'], 'fee':data.at[nRow,'fee'], 'weight':data.at[nRow,'weight'],'parents':str(data.at[nRow,'parents '])}
+                    df=df.append(new_Row, ignore_index = True)                
+
+        new_Row={'tx_id':data.at[row,'tx_id'], 'fee':data.at[row,'fee'], 'weight':data.at[row,'weight'],'parents':str(data.at[row,'parents '])}
+        df=df.append(new_Row, ignore_index = True)
+    else:
+        new_Row={'tx_id':data.at[row,'tx_id'], 'fee':data.at[row,'fee'], 'weight':data.at[row,'weight'],'parents':str(data.at[row,'parents '])}
+        df=df.append(new_Row, ignore_index = True)
+
+
+def recurssive_checker():
+    changed=False
+    for row in data.index:
+        if(changed==False):
+            if(check_list(data.at[row,'tx_id'])==True):
+                continue
+            else:
+                for r in df.index:
+                    if(max_weight>=(total_weight-df.at[r,'weight']+data.at[row,'weight'])):
+                        temp_fees=current_fees-df.at[r,'fee']+data.at[row,'fee']
+                        if(temp_fees>current_fees):
+                            df = df.drop(labels=r, axis=0)
+                            new_Row={'tx_id':data.at[row,'tx_id'], 'fee':data.at[row,'fee'], 'weight':data.at[row,'weight'],'parents':str(data.at[row,'parents '])}
+                            df=df.append(new_Row, ignore_index = True)
+                            changed=True
+                            recurssive_checker()
+        else:
+            break
+            
+        
+
+    
+
+total_weight=0
+current_fees=0
+max_weight=4000000
+
+df = pd.DataFrame(columns=['tx_id', 'fee', 'weight','parents '])
+# Created a basic block filled with transactions
+for row in data.index:
+    if(total_weight+data.at[row,'weight']<=max_weight):
+        total_weight=total_weight+data.at[row,'weight']
+        current_fees=current_fees+data.at[row,'fee']
+        checkParents(row)
+
+
+for row in data.index:
+    if(check_list(data.at[row,'tx_id'])==True):
+        continue
+    else:
+        for r in df.index:
+            if(max_weight>=(total_weight-df.at[r,'weight']+data.at[row,'weight'])):
+                temp_fees=current_fees-df.at[r,'fee']+data.at[row,'fee']
+                if(temp_fees>current_fees):
+                    df = df.drop(labels=r, axis=0)
+                    new_Row={'tx_id':data.at[row,'tx_id'], 'fee':data.at[row,'fee'], 'weight':data.at[row,'weight'],'parents':str(data.at[row,'parents '])}
+                    df=df.append(new_Row, ignore_index = True)
+
+
+
+print(df)
+print(total_weight)
+print(current_fees)
+
+# with open('block.txt', 'w') as filehandle:
+# 	for r in df.index:
+# 		filehandle.write('%s\n'+str(df.at[row,'tx_id']))
+
+
---
+++ challenge1.py
@@ -0,0 +1,55 @@
+import pandas as pd
+
+data = pd.read_csv("mempool.csv") 
+
+# print(data)
+
+
+global max_weight=4000000
+global total_weight=0
+global current_fees=0
+
+# def parent_checker(row):
+#     if str(data.at[row,'parents']) != "nan":
+#         parent_list = str(data.at[row,'parents']).split(";")
+#         for i in parent_list:
+#             if(check_list(i)):
+#                 continue
+#             else:
+#                 txnindex = df[df['tx_id'] == i].index.item()
+#                 k = df.loc[txnindex]
+#                 check_add_txn(k)
+
+# print(type(max_weight))
+# print(len(data))
+# Adding number transactions till the block is filled
+for row in data.index:
+    if(total_weight+data.at[row,'weight']<=max_weight):
+        total_weight=total_weight+data.at[row,'weight']
+        current_fees=current_fees+data.at[row,'fee']
+        # Todo add transactions here
+
+print(total_weight)
+print(current_fees)
+
+
+# Todo create a  parent checker list
+# If parennt added continue else add the paprent ------> recusrive funtion to check for parents
+# Add transactions
+# replace better values with worse ones
+# Once a value has been changed try to iterate all value
+#  only do after precaution
+
+
+# print(data.info)
+# print(data.dtypes)
+# print(data.describe)
+# # def addToBlock(total_weight, weight , fees , tx_id):
+# # 	if((total_weight + weight < max_weight)):
+# # 		transactions.append(tx_id)
+# # 		TOTAL_WEIGTH = TOTAL_WEIGTH + int(weight)
+# # 		max_fees = max_fees + int(fees)
+		
+# # 		return True
+# # 	else:
+# # 		return False
